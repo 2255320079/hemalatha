ed5f0b0e548d4486ff654dcc7c8ae3632b93672a
coding journey <125361677+codingjourney1@users.noreply.github.com>
2023-02-24 10:31:29 +0530

    Add files via upload

---
+++ even or odd_+ve or-ve.c
@@ -0,0 +1,25 @@
+//even or odd number
+#include<stdio.h>
+main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n);
+  if(n%2==0)
+  {
+    printf("%d is even number",n); 
+  }
+  else
+  {
+    printf("%d is odd number",n);
+  }
+//positive number
+if(n>0)
+{
+printf("%d is positive",n);
+}
+else
+{
+printf("%d is negative",n);
+}
+//
